8d8ce8b4525f6f2f095945c27a35026305b4f25b
4tronix <Sales@4tronix.co.uk>
2018-05-11 16:35:50 +0100

    COnvert to Robobit
    First conversion to robobit pins and functions

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-
+﻿
 /**
   * Enumeration of motors.
   */
@@ -42,7 +42,7 @@ enum BBLightSensor {
 }
 
 /**
- * Ping unit for sesnor.
+ * Ping unit for sensor
  */
 enum BBPingUnit {
     //% block="μs"
@@ -57,29 +57,14 @@ enum BBPingUnit {
  * Custom blocks
  */
 //% weight=10 color=#0fbc11 icon="\uf1b9"
-namespace bitbot {
-
-    let neoStrip: neopixel.Strip;
-
-    /**
-     * Return a neo pixel strip.
-     */
-    //% blockId="bitbot_neo" block="neo strip"
-    //% weight=5
-    export function neo(): neopixel.Strip {
-        if (!neoStrip) {
-            neoStrip = neopixel.create(DigitalPin.P13, 12, NeoPixelMode.RGB)
-        }
-
-        return neoStrip;
-    }
+namespace robobit {
 
     /**
       * Drive robot forward (or backward) at speed.
       *
       * @param speed speed of motor between -1023 and 1023.
       */
-    //% blockId="bitbot_motor_forward" block="drive at speed %speed"
+    //% blockId="robobit_motor_forward" block="drive at speed %speed"
     //% speed.min=-1023 speed.max=1023
     //% weight=110
     export function drive(speed: number): void {
@@ -92,7 +77,7 @@ namespace bitbot {
       * @param speed speed of motor between -1023 and 1023.
       * @param milliseconds duration in milliseconds to drive forward for, then stop.
       */
-    //% blockId="bitbot_motor_forward_milliseconds" block="drive at speed %speed| for milliseconds %milliseconds"
+    //% blockId="robobit_motor_forward_milliseconds" block="drive at speed %speed| for milliseconds %milliseconds"
     //% speed.min=-1023 speed.max=1023
     //% weight=131
     export function driveMilliseconds(speed: number, milliseconds: number): void {
@@ -107,7 +92,7 @@ namespace bitbot {
       * @param direction direction to turn.
       * @param speed speed of motor between 0 and 1023.
       */
-    //% blockId="bitbot_turn" block="turn in direction %direction|speed %speed"
+    //% blockId="robobit_turn" block="turn in direction %direction|speed %speed"
     //% speed.min=0 speed.max=1023
     //% weight=109
     export function driveTurn(direction: BBRobotDirection, speed: number): void {
@@ -129,7 +114,7 @@ namespace bitbot {
       * @param speed speed of motor between 0 and 1023.
       * @param milliseconds duration in milliseconds to turn for, then stop.
       */
-    //% blockId="bitbot_turn_milliseconds" block="turn in direction %direction|speed %speed| for milliseconds %milliseconds"
+    //% blockId="robobit_turn_milliseconds" block="turn in direction %direction|speed %speed| for milliseconds %milliseconds"
     //% speed.min=0 speed.max=1023
     //% weight=130
     export function driveTurnMilliseconds(direction: BBRobotDirection, speed: number, milliseconds: number): void {
@@ -144,7 +129,7 @@ namespace bitbot {
       * @param motor motor to drive.
       * @param speed speed of motor
       */
-    //% blockId="bitbot_motor" block="drive motor %motor|speed %speed"
+    //% blockId="robobit_motor" block="drive motor %motor|speed %speed"
     //% weight=100
     export function motor(motor: BBMotor, speed: number): void {
         let forward = (speed >= 0);
@@ -180,23 +165,11 @@ namespace bitbot {
     }
 
     /**
-      * Sound a buzz.
-      *
-      * @param flag Flag to set (0) for off and (1) for on.
-      */
-    //% blockId="bitbot_buzz" block="buzz sound %flag"
-    //% flag.min=0 flag.max=1
-    //% weight=95
-    export function buzz(flag: number): void {
-        pins.digitalWritePin(DigitalPin.P14, flag === 0 ? 0 : 1);
-    }
-
-    /**
       * Read line sensor.
       *
       * @param sensor Line sensor to read.
       */
-    //% blockId="bitbot_read_line" block="read line sensor %sensor"
+    //% blockId="robobit_read_line" block="read line sensor %sensor"
     //% weight=90
     export function readLine(sensor: BBLineSensor): number {
         if (sensor == BBLineSensor.Left) {
@@ -206,114 +179,18 @@ namespace bitbot {
         }
     }
 
-    /**
-      * Read light sensor.
-      *
-      * @param sensor Light sensor to read.
-      */
-    //% blockId="bitbot_read_light" block="read light sensor %sensor"
-    //% weight=90
-    export function readLight(sensor: BBLightSensor): number {
-        if (sensor == BBLightSensor.Left) {
-            pins.digitalWritePin(DigitalPin.P16, 0);
-            return pins.analogReadPin(AnalogPin.P2);
-        } else {
-            pins.digitalWritePin(DigitalPin.P16, 1);
-            return pins.analogReadPin(AnalogPin.P2);
-        }
-    }
-
-    /**
-      * Shows all LEDs to a given color (range 0-255 for r, g, b).
-      *
-      * @param rgb RGB color of the LED
-      */
-    //% blockId="bitbot_neo_set_color" block="set pixels to %rgb=neopixel_colors"
-    //% weight=80
-    export function neoSetColor(rgb: number) {
-        neo().showColor(rgb);
-    }
-
-    /**
-     * Set LED to a given color (range 0-255 for r, g, b).
-     *
-     * @param offset position of the NeoPixel in the strip
-     * @param rgb RGB color of the LED
-     */
-    //% blockId="bitbot_neo_set_pixel_color" block="set pixel color at %offset|to %rgb=neopixel_colors"
-    //% weight=80
-    export function neoSetPixelColor(offset: number, rgb: number): void {
-        neo().setPixelColor(offset, rgb);
-    }
-
-    /**
-      * Show leds.
-      */
-    //% blockId="bitbot_neo_show" block="show leds"
-    //% weight=76
-    export function neoShow(): void {
-        neo().show();
-    }
-
-    /**
-      * Clear leds.
-      */
-    //% blockId="bitbot_neo_clear" block="clear leds"
-    //% weight=75
-    export function neoClear(): void {
-        neo().clear();
-    }
-
-    /**
-      * Shows a rainbow pattern on all LEDs.
-      */
-    //% blockId="bitbot_neo_rainbow" block="set led rainbow"
-    //% weight=70
-    export function neoRainbow(): void {
-        neo().showRainbow(1, 360);
-    }
-
-    /**
-     * Shift LEDs forward and clear with zeros.
-     */
-    //% blockId="bitbot_neo_shift" block="shift led pixels"
-    //% weight=66
-    export function neoShift(): void {
-        neo().shift(1);
-    }
-
-    /**
-     * Rotate LEDs forward.
-     */
-    //% blockId="bitbot_neo_rotate" block="rotate led pixels"
-    //% weight=65
-    export function neoRotate(): void {
-        neo().rotate(1);
-    }
-
-    /**
-     * Set the brightness of the strip. Note this only applies to future writes to the strip.
-     *
-     * @param brightness a measure of LED brightness in 0-255. eg: 255
-     */
-    //% blockId="bitbot_neo_brightness" block="set led brightness %brightness"
-    //% brightness.min=0 brightness.max=255
-    //% weight=10
-    export function neoBrightness(brightness: number): void {
-        neo().setBrightness(brightness);
-    }
 
     /**
     * Read distance from sonar module connected to accessory connector.
     *
     * @param unit desired conversion unit
     */
-    //% blockId="bitbot_sonar" block="read sonar as %unit"
+    //% blockId="robobit_sonar" block="read sonar as %unit"
     //% weight=7
     export function sonar(unit: BBPingUnit): number {
         // send pulse
-        let trig = DigitalPin.P15;
-        let echo = DigitalPin.P15;
+        let trig = DigitalPin.P13;
+        let echo = DigitalPin.P13;
 
         let maxCmDistance = 500;
 
